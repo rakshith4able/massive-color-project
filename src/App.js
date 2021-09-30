bdd54d8b8ed937fbf48a7320e9ea9aef20e0e314
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-09-30 20:29:36 +0530

    Adding Slider to control color levels using rc-slider library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { generatePalette } from './ColorHelpers';
 import Palette from './Palette';
 import seedColor from './seedColor';
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedColor[4]} />
+      <Palette palette={generatePalette(seedColor[4])} />
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css'
 import './Palette.css';
 
 class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            level: 500
+        };
+        this.changelevel = this.changelevel.bind(this);
+    }
+
+    changelevel(level) {
+        this.setState({ level: level });
+    }
+
     render() {
 
-        const colorBoxes = this.props.colors.map(
+        const colorBoxes = this.props.palette.colors[this.state.level].map(
             (color) => {
-                return <ColorBox background={color.color} name={color.name} />
+                return <ColorBox background={color.hex} name={color.name} />
             }
         );
 
         return (
             <div className="Palette">
+                <Slider
+                    defaultValue={this.state.level}
+                    min={100}
+                    max={900}
+                    step={100}
+                    onAfterChange={this.changelevel}
+                />
+
                 {/* header */}
                 <div className="Palette-colors">
                     {/* Colors */}
