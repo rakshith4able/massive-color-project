c977014c6e0ac22d703c4ac1f2e570913e76dbaf
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-10-02 18:50:08 +0530

    Added Link to More button in colorbox and added Route

--- src/App.js
+++ src/App.js
@@ -23,6 +23,10 @@ class App extends Component {
             <Palette palette={generatePalette(this.findPalette(args.match.params.id))}
             />)}
         />
+        <Route exact
+          path="/palette/:paletteId/:colorId"
+          render={() => <h1>New Color Page</h1>}
+        />
       </Switch >
 
       // <div className="App">
