43e91aff263935adf550e6fa4b138ea4e4fd730d
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-10-06 10:32:07 +0530

    adjusted text colors based on lumiousity

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 import './ColorBox.css';
 
 class ColorBox extends Component {
@@ -21,6 +22,8 @@ class ColorBox extends Component {
     render() {
         const { name, background, moreLink, showLink } = this.props;
         const { copied } = this.state;
+        const isDarkColor = chroma(background).luminance() <= 0.08;
+        const isLightColor = chroma(background).luminance() >= 0.8;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div className="ColorBox" style={{ backgroundColor: background }}>
@@ -31,16 +34,16 @@ class ColorBox extends Component {
                     </div>
                     <div className="copy-container">
                         <div className="box-content">
-                            <span>{name}</span>
+                            <span className={isDarkColor && "light-text"}>{name}</span>
                         </div>
-                        <button className="copy-button">Copy</button>
+                        <button className={`copy-button ${isLightColor && 'dark-text'}`}>Copy</button>
                     </div>
                     {showLink &&
                         <Link to={moreLink} onClick={e => e.stopPropagation()}>
-                            <span className="see-more">More</span>
+                            <span className={`see-more ${isLightColor && 'dark-text'}`}>MORE</span>
                         </Link>}
                 </div>
-            </CopyToClipboard>
+            </CopyToClipboard >
         )
     }
 }
