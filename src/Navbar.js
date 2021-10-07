d92d2ea1faaf29de0bc1d02915dfd258aaf6ba06
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-10-07 11:36:18 +0530

    Refactored Navbar styles and removed all css

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,60 +6,57 @@ import Snackbar from '@mui/material/Snackbar';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import 'rc-slider/assets/index.css'
-import './Navbar.css';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@mui/styles';
+
+import styles from "./Styles/NavbarStyles";
+
 class Navbar extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            format: "hex",
-            open: false
-        };
+        this.state = { format: "hex", open: false };
         this.handleFormatChange = this.handleFormatChange.bind(this);
         this.closeSnackbar = this.closeSnackbar.bind(this);
     }
-
-    handleFormatChange(evt) {
-        this.setState({ format: evt.target.value, open: true });
-        this.props.handleChange(evt.target.value);
+    handleFormatChange(e) {
+        this.setState({ format: e.target.value, open: true });
+        this.props.handleChange(e.target.value);
     }
-
     closeSnackbar() {
         this.setState({ open: false });
     }
-
     render() {
-        const { level, changeLevel, isSinglePalette } = this.props;
-        const { format, open } = this.state;
+        const { level, changeLevel, showingAllColors, classes } = this.props;
+        const { format } = this.state;
         return (
-            <header className="Navbar">
-                <div className="logo">
-                    <Link to="/">
-                        reactcolorpicker
-                    </Link>
+            <header className={classes.Navbar}>
+                <div className={classes.logo}>
+                    <Link to='/'>reactcolorpicker</Link>
                 </div>
-                {!isSinglePalette && <div className="slider-container">
-                    <span>Level:{level}</span>
-                    <div className='slider'>
-                        <Slider
-                            defaultValue={level}
-                            min={100}
-                            max={900}
-                            step={100}
-                            onAfterChange={changeLevel}
-                        />
+                {showingAllColors && (
+                    <div>
+                        <span>Level: {level}</span>
+                        <div className={classes.slider}>
+                            <Slider
+                                defaultValue={level}
+                                min={100}
+                                max={900}
+                                step={100}
+                                onAfterChange={changeLevel}
+                            />
+                        </div>
                     </div>
-                </div>}
-                <div className="select-container">
+                )}
+                <div className={classes.selectContainer}>
                     <Select value={format} onChange={this.handleFormatChange}>
-                        <MenuItem value="hex">HEX - #ffffff</MenuItem>
-                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
-                        <MenuItem value="rgba">RGBA - rgba(255,255,255,1)</MenuItem>
+                        <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+                        <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value='rgba'>RGBA - rgba(255,255,255, 1.0)</MenuItem>
                     </Select>
                 </div>
                 <Snackbar
                     anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-                    open={open}
+                    open={this.state.open}
                     autoHideDuration={3000}
                     message={
                         <span id='message-id'>
@@ -82,8 +79,7 @@ class Navbar extends Component {
                     ]}
                 />
             </header>
-        )
+        );
     }
 }
-
-export default Navbar;
+export default withStyles(styles)(Navbar);
