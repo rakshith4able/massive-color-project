c6655a6ec35befd17add04d7e49ed11351daca28
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-10-18 13:10:30 +0530

    Made Palette List resoponsive

--- src/Styles/sizes.js
+++ src/Styles/sizes.js
@@ -5,9 +5,9 @@ export default {
             xs: "575.98px",
             sm: "767.98px",
             md: "991.98px",
-            lg: "1199.98px"
+            lg: "1199.98px",
+            xl: "1600px"
         };
         return `@media (max-width: ${sizes[size]})`;
     }
 };
-
