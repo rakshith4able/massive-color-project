9908ec3f63549c574f7a43583572edfe627a47da
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-10-18 13:39:38 +0530

    Added Palette Delete confirmation dialogue

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 import { withStyles } from "@mui/styles";
 import styles from "./Styles/PaletteListStyles";
+import blue from "@material-ui/core/colors/blue";
+import red from "@material-ui/core/colors/red";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 class PaletteList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openDeleteDialog: false,
+            deletingId: ""
+        };
+        this.openDialog = this.openDialog.bind(this);
+        this.closeDialog = this.closeDialog.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    openDialog(id) {
+        this.setState({ openDeleteDialog: true, deletingId: id });
+    }
+    closeDialog() {
+        this.setState({ openDeleteDialog: false, deletingId: "" });
+    }
     goToPalette(id) {
         this.props.history.push(`/palette/${id}`);
     }
+    handleDelete() {
+        this.props.deletePalette(this.state.deletingId);
+        this.closeDialog();
+    }
     render() {
         const { palettes, classes, deletePalette } = this.props;
+        const { openDeleteDialog, deletingId } = this.state;
         return (
             <div className={classes.root}>
                 <div className={classes.container}>
@@ -24,7 +56,8 @@ class PaletteList extends Component {
                                 <MiniPalette
                                     {...palette}
                                     handleClick={() => this.goToPalette(palette.id)}
-                                    handleDelete={deletePalette}
+                                    // handleDelete={deletePalette}
+                                    openDialog={this.openDialog}
                                     key={palette.id}
                                     id={palette.id}
                                 />
@@ -32,6 +65,35 @@ class PaletteList extends Component {
                         ))}
                     </TransitionGroup>
                 </div>
+                <Dialog
+                    open={openDeleteDialog}
+                    aria-labelledby='delete-dialog-title'
+                    onClose={this.closeDialog}
+                >
+                    <DialogTitle id='delete-dialog-title'>
+                        Delete This Palette?
+                    </DialogTitle>
+                    <List>
+                        <ListItem button onClick={this.handleDelete}>
+                            <ListItemAvatar>
+                                <Avatar
+                                    style={{ backgroundColor: blue[100], color: blue[600] }}
+                                >
+                                    <CheckIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary='Delete' />
+                        </ListItem>
+                        <ListItem button onClick={this.closeDialog}>
+                            <ListItemAvatar>
+                                <Avatar style={{ backgroundColor: red[100], color: red[600] }}>
+                                    <CloseIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary='Cancel' />
+                        </ListItem>
+                    </List>
+                </Dialog>
             </div>
         );
     }
